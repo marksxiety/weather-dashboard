030688381c5af6c33d8a820ea0429297996aa7fe
Mark Ciril <markciril.jamon@gmail.com>
2024-11-25 23:41:43 +0800

    Chore: adjust the padding and remove the shadow

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col min-h-24 rounded-lg border border-solid shadow-md">
-      <slot></slot>
+    <div class="flex flex-col min-h-24 rounded-lg border border-solid p-4">
+        <slot></slot>
     </div>
-  </template>
+</template>
