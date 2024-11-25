4f2fe103751dd3187334fdabc5a9cc058ac8ada0
Mark Ciril <markciril.jamon@gmail.com>
2024-11-25 23:07:45 +0800

    INIT: install dependencies and set up project tech stack

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="font-sans">
+    <RouterView />
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1 class="text-xl">test</h1>
+  </main>
+</template>
+
+<script setup>
+</script>
