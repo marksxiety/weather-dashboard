168182ca1beddbb85fab66bf79934b9f23233c87
Mark Ciril <markciril.jamon@gmail.com>
2024-11-25 23:44:05 +0800

    Feat: initial grid structure for the homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,19 @@
 <template>
-  <main>
-    <h1 class="text-xl">test</h1>
-  </main>
+  <div class="p-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 place-items-center gap-4">
+    <Card>
+      <CardHeader>
+        Card Title
+      </CardHeader>
+      <CardContent>
+        <p>This is the main content of the card.</p>
+      </CardContent>
+    </Card>
+  </div>
 </template>
 
 <script setup>
+import Card from '@/components/Card/Card.vue'
+import CardHeader from '@/components/Card/CardHeader.vue'
+import CardContent from '@/components/Card/CardContent.vue'
+import CardFooter from '@/components/Card/CardFooter.vue'
 </script>
