868cf603cfc8a92e0c15b8aae1121f2fceb8a12f
Mark Ciril <markciril.jamon@gmail.com>
2024-11-28 01:04:04 +0800

    Chore: create a reusable component for Weather metrics based on passed props

---
+++ src/components/Section/WeatherMetrics.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex items-center space-x-2">
+      <Icon :icon="logo" :class="[height, width, logocolor]" />
+      <div class="flex flex-col">
+        <p :class="valueClass">{{ value }}</p>
+        <p :class="labelClass">{{ label }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { Icon } from '@iconify/vue'
+  const props = defineProps({
+    logo: {
+      default: 'weui:error-outlined',
+      type: String
+    },
+    label: {
+      default: 'N/A',
+      type: String
+    },
+    value: {
+      default: '-',
+      type: String
+    },
+    logocolor: {
+      default: 'text-black-400',
+      type: String
+    },
+    height: {
+      default: 'h-8',
+      type: String
+    },
+    width: {
+      default: 'w-8',
+      type: String
+    },
+    labelClass: {
+      default: 'text-xs text-black-400',
+      type: String
+    },
+    valueClass: {
+      default: 'text-sm font-semibold',
+      type: String
+    }
+  })
+  </script>  
