a49fed9277ddd7901b31fec60eca86e37afcd3b2
Mark Ciril <markciril.jamon@gmail.com>
2024-11-29 05:05:35 +0800

    Chore: add color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       },
+      colors: {
+        primary: '#3E2522',
+        secondary: '#8C6E63',
+        tertiary: '#D3A376',
+        accent: '#FFE0B2',
+        light: '#FFF2DF',
+      },
     },
   },
   plugins: [],
