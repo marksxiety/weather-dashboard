99b87f16ce313487503174f05ee27b6e7b5a59bf
Mark Ciril <markciril.jamon@gmail.com>
2024-12-02 17:43:36 +0800

    Design: adjust some padding and color of search input in navigation bar

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="min-h-14 border-b-2 shadow-sm flex justify-between gap-2 ml-4 mr-4 p-2">
-       <div class="grid place-content-center font-semibold">
-        <h2>CloudSnap</h2>
-       </div>
-        <div>
-            <div class="mt-2">
-                <div
-                    class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                    <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6"><Icon icon="lucide:search" /></div>
-                    <input type="text" name="price" id="price"
-                        class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                        placeholder="cities here..." />
-                </div>
-            </div>
+    <div class="min-h-14 border-b-2 shadow-sm flex justify-between gap-4 pb-4 pt-4 pl-6 pr-6">
+        <div class="grid place-content-center font-semibold">
+            <h2>CloudSnap</h2>
         </div>
-        <div class="flex justify-between gap-2 items-center">
-            <NavbarCard><Icon icon="solar:settings-outline" class="w-8 h-6"/></NavbarCard>
-            <NavbarCard><Icon icon="icon-park-outline:add" class="w-8 h-6"/></NavbarCard>
+        <div
+            class="flex items-center rounded-2xl pl-3 w-1/4 outline outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-primary">
+            <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">
+                <Icon icon="lucide:search" />
+            </div>
+            <input type="text" name="price" id="price"
+                class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                placeholder="cities here..." />
+        </div>
+        <div class="flex justify-between gap-4 items-center">
+            <NavbarCard>
+                <Icon icon="solar:settings-outline" class="w-8 h-6" />
+            </NavbarCard>
+            <NavbarCard>
+                <Icon icon="icon-park-outline:add" class="w-8 h-6" />
+            </NavbarCard>
         </div>
     </div>
 </template>
