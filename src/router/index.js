5406e84f9821df24d0380f929bae997d0714f0d7
Mark Ciril <markciril.jamon@gmail.com>
2024-12-03 23:21:18 +0800

    Feat: add a routing for city info using dynamic value for specfic city

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CityListings from '../views/CityListings.vue'
+import CityListings from '@/views/CityListings.vue'
+import CityInfo from '@/views/CityInfo.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'citylistings',
       component: CityListings,
+    },
+    {
+      path: '/city/:city',
+      name: 'cityinfo',
+      component: CityInfo,
     }
   ],
 })
