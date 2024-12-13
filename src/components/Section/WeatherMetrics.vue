d35ffad5d796f985e4e5d93ec9c837443e114435
Mark Ciril <markciril.jamon@gmail.com>
2024-12-13 17:55:15 +0800

    Refactor: add imgfirst and justify content direction as props to have a dynamic usage of weathermetrics component

--- src/components/Section/WeatherMetrics.vue
+++ src/components/Section/WeatherMetrics.vue
@@ -1,47 +1,67 @@
 <template>
-    <div class="flex flex-row gap-x-4 w-full">
-      <Icon :icon="logo" :class="[height, width, logocolor]" />
+  <div 
+    class="flex gap-x-4 w-full" 
+    :class="['flex-row', justifyContent]"
+  >
+    <template v-if="imgFirst">
+      <img :src="logo" alt="Weather Information" :class="[height, width]" />
       <div class="flex flex-col">
         <p :class="valueClass">{{ value }}</p>
         <p :class="labelClass">{{ label }}</p>
       </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { Icon } from '@iconify/vue'
-  const props = defineProps({
-    logo: {
-      default: 'weui:error-outlined',
-      type: String
-    },
-    label: {
-      default: 'N/A',
-      type: String
-    },
-    value: {
-      default: '-',
-      type: String
-    },
-    logocolor: {
-      default: 'text-black-400',
-      type: String
-    },
-    height: {
-      default: 'h-8',
-      type: String
-    },
-    width: {
-      default: 'w-8',
-      type: String
-    },
-    labelClass: {
-      default: 'text-xs text-black-400',
-      type: String
-    },
-    valueClass: {
-      default: 'text-sm font-semibold',
-      type: String
-    }
-  })
-  </script>  
+    </template>
+    <template v-else>
+      <div class="flex flex-col">
+        <p :class="valueClass">{{ value }}</p>
+        <p :class="labelClass">{{ label }}</p>
+      </div>
+      <img :src="logo" alt="Weather Information" :class="[height, width]" />
+    </template>
+  </div>
+</template>
+
+
+
+<script setup>
+import not_found from '@/assets/weather_logo/not_found.png';
+
+const props = defineProps({
+  logo: {
+    default: not_found,
+    type: String,
+  },
+  label: {
+    default: 'N/A',
+    type: String,
+  },
+  value: {
+    default: '-',
+    type: [String, Number],
+  },
+  imgFirst: {
+    default: true, // Determines the order of img and div
+    type: Boolean,
+  },
+  justifyContent: {
+    default: 'justify-start',
+    type: String,
+  },
+  height: {
+    default: 'h-8',
+    type: String,
+  },
+  width: {
+    default: 'w-8',
+    type: String,
+  },
+  labelClass: {
+    default: 'text-xs text-black-400',
+    type: String,
+  },
+  valueClass: {
+    default: 'text-sm font-semibold',
+    type: String,
+  },
+});
+
+</script>
