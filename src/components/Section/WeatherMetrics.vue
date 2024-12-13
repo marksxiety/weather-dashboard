a71993528ca42e79e73f13957cbfa0d1c00351e4
Mark Ciril <markciril.jamon@gmail.com>
2024-12-13 22:49:12 +0800

    FEAT: add media query in font-size of <p> elements

--- src/components/Section/WeatherMetrics.vue
+++ src/components/Section/WeatherMetrics.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="grid place-content-center">
-    <div 
-    class="flex gap-x-4 w-full" 
-    :class="['flex-row', justifyContent]"
-  >
-    <template v-if="imgFirst">
-      <img :src="logo" alt="Weather Information" :class="[height, width]" />
-      <div class="flex flex-col">
-        <p :class="valueClass">{{ value }}</p>
-        <p :class="labelClass">{{ label }}</p>
-      </div>
-    </template>
-    <template v-else>
-      <div class="flex flex-col">
-        <p :class="valueClass">{{ value }}</p>
-        <p :class="labelClass">{{ label }}</p>
-      </div>
-      <img :src="logo" alt="Weather Information" :class="[height, width]" />
-    </template>
+    <div class="flex gap-x-4 w-full" :class="['flex-row', justifyContent]">
+      <template v-if="imgFirst">
+        <img :src="logo" alt="Weather Information" :class="[height, width]" />
+        <div class="flex flex-col">
+          <p :class="valueClass">{{ value }}</p>
+          <p :class="labelClass">{{ label }}</p>
+        </div>
+      </template>
+      <template v-else>
+        <div class="flex flex-col">
+          <p :class="valueClass">{{ value }}</p>
+          <p :class="labelClass">{{ label }}</p>
+        </div>
+        <img :src="logo" alt="Weather Information" :class="[height, width]" />
+      </template>
+    </div>
   </div>
-  </div>
- 
 </template>
-
-
 
 <script setup>
 import not_found from '@/assets/weather_logo/not_found.png';
@@ -62,15 +56,9 @@ const props = defineProps({
     type: String,
   },
   valueClass: {
-    default: 'text-sm font-semibold',
+    default: 'text-sm font-semibold md:text-md lg:text-lg',
     type: String,
   },
 });
 
 </script>
-
-<style scoped>
-p{
-  font-size: small;
-}
-</style>
