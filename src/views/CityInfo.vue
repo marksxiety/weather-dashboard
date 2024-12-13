d2b4ce1a2cd6ad3ee525d0e7ca1aa4703b8d2821
Mark Ciril <markciril.jamon@gmail.com>
2024-12-13 22:22:33 +0800

    Feat: include the navbar in the cityinfo vue

--- src/views/CityInfo.vue
+++ src/views/CityInfo.vue
@@ -1,4 +1,5 @@
 <template>
+  <Navbar/>
     <div class="p-8">
       <h1 class="text-3xl font-bold mb-4">City Info: {{ city || 'Not Found' }}</h1>
     </div>
@@ -6,7 +7,7 @@
   
   <script setup>
   import { useRoute } from 'vue-router';
-  import { useWeatherStore } from '@/stores/weatherStore';
+  import Navbar from '@/components/Navbar/Navbar.vue';
   
   const route = useRoute();
   
