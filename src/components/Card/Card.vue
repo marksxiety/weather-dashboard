fabf6019965fd800c060812cec3ad2a3d65d7490
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 03:12:19 +0800

    feat: props for card to have a dynamic border and bg color

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="flex flex-col h-full w-full rounded-lg border-2 p-4 border-primary bg-background">
-      <slot></slot>
-    </div>
-  </template>
-  
+  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4" :class="[`border-${bordercolor}`, `bg-${bgcolor}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  bordercolor: {
+    type: String,
+    default: 'primary',
+  },
+  bgcolor: {
+    type: String,
+    default: 'background',
+  },
+})
+</script>
