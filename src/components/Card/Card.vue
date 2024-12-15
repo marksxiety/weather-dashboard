6932ded20d98a569edb98cc1790e9d16b5bcd6a7
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 03:17:11 +0800

    fix: add props. to access the passed props parameter

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4" :class="[`border-${bordercolor}`, `bg-${bgcolor}`]">
+  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4" :class="[`border-${props.bordercolor}`, `bg-${props.bgcolor}`]">
     <slot></slot>
   </div>
 </template>
