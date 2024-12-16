3b74309842d56331c85e3713fc15df77ea2366b0
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 15:52:32 +0800

    feat: add setting a selected city weather that can be accessed and change the value when clicing a certain card (city) to easily access the specific city that is accessed by the user

--- src/stores/weatherStore.js
+++ src/stores/weatherStore.js
@@ -1,27 +1,33 @@
-import { defineStore } from "pinia";
-import axiosConfig from "@/plugins";
+import { defineStore } from "pinia"
+import axiosConfig from "@/plugins"
 
 export const useWeatherStore = defineStore("weatherStore", {
   state: () => ({
-    data: [],
+    // initial values for weather state
+    data: [], 
+    selectedCityWeather: null,
     loading: false,
     error: null,
   }),
   actions: {
     async loadCityWeather(country, city, apiKey) {
-      this.loading = true;
-      this.error = null;
+      this.loading = true
+      this.error = null
       try {
         const response = await axiosConfig.get(
           `/weather?q=${city},${country}&appid=${apiKey}&units=metric`
-        );
-        return response.data;
+        )
+        return response.data
       } catch (err) {
-        this.error = err.message;
-        throw err; 
+        this.error = err.message
+        throw err
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     },
+    ///ensure that the weather data for the specific city is stored in a dedicated place
+    setSelectedCityWeather(cityWeatherData) {
+      this.selectedCityWeather = cityWeatherData
+    },
   },
-});
+})
