96736363cf625fa9bff47d3918b3df85913e686d
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 20:18:12 +0800

    chore: remove the props for card and retain the default card border

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,27 +1,5 @@
 <template>
-  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4"
-       :class="computedClasses">
+  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4 bg-background">
     <slot></slot>
   </div>
 </template>
-
-<script setup>
-import { computed } from 'vue'
-
-const props = defineProps({
-  bordercolor: {
-    type: String,
-    default: 'primary',
-  },
-  bgcolor: {
-    type: String,
-    default: 'background',
-  },
-})
-
-// Computed class
-const computedClasses = computed(() => [
-  `border-${props.bordercolor}`,
-  `bg-${props.bgcolor}`,
-])
-</script>
