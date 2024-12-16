c7deac95d7154268a3a86877e37c1b46615972eb
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 20:32:53 +0800

    feat: initial structure of card

--- src/views/CityInfo.vue
+++ src/views/CityInfo.vue
@@ -1,18 +1,35 @@
 <template>
   <Navbar />
-  <div v-if="cityWeather">
-    <pre>{{ cityWeather }}</pre>
+  <div v-if="cityWeather && isLoaded" class="p-4">
+    <div class="grid grid-cols-3 gap-4">
+      <div class="flex flex-col col-span-2 min-h-screen gap-4">
+        <Card class="h-full">
+          test
+        </Card>
+        <Card class="h-full">
+          test
+        </Card>
+      </div>
+      <Card class="col-span-1 h-full min-h-screen">
+        test
+      </Card>
+    </div>
+  </div>
+  <div v-else class="grid items-center text-center min-h-screen">
+    <p>Loading data. Please wait...</p>
   </div>
 </template>
 
 <script setup>
 import { useWeatherStore } from '@/stores/weatherStore'
-import { onMounted, computed } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import Navbar from '@/components/Navbar/Navbar.vue'
+import Card from '@/components/Card/Card.vue'
 
 const weatherStore = useWeatherStore()
 const route = useRoute()
+const isLoaded = ref(false)
 
 const cityWeather = computed(() => weatherStore.selectedCityWeather)
 
@@ -26,7 +43,8 @@ onMounted(async () => {
     // fetch the city information
     const weatherData = await weatherStore.loadCityWeather(country, cityName, apiKey)
     // Store the fetched data in the store
-    weatherStore.setSelectedCityWeather(weatherData) 
+    weatherStore.setSelectedCityWeather(weatherData)
+    isLoaded.value = true
   } catch (error) {
     // throw an error in case the fethcing is not successful
     console.error('error fetching city weather', cityWeather)
