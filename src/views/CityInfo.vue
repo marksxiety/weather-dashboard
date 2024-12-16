65a61246cde47fa60df1c6e89a11bf99d493f78b
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 15:01:55 +0800

    feat: access the passed parameter by definiting it

--- src/views/CityInfo.vue
+++ src/views/CityInfo.vue
@@ -1,17 +1,19 @@
 <template>
-  <Navbar/>
-    <div class="p-8">
-      <h1 class="text-3xl font-bold mb-4">City Info: {{ city || 'Not Found' }}</h1>
-    </div>
-  </template>
-  
-  <script setup>
-  import { useRoute } from 'vue-router';
-  import Navbar from '@/components/Navbar/Navbar.vue';
-  
-  const route = useRoute();
-  
-  // Access the `city` parameter from the route
-  const city = route.params.city;
-  </script>
-  
+  <Navbar />
+  <div class="p-8">
+    <h1 class="text-3xl font-bold mb-4">City Info: {{ data?.name || 'Not Found' }}</h1>
+    <pre>{{ data }}</pre> 
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import Navbar from '@/components/Navbar/Navbar.vue'
+
+const props = defineProps({
+  city: String,
+  data: Object
+})
+
+console.log(props)
+</script>
