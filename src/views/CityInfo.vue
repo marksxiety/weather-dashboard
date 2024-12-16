6be3459a3dc00840cab5f5e97184bd3ae435eca5
Mark Ciril <markciril.jamon@gmail.com>
2024-12-16 15:54:04 +0800

    feat: access the specific city weather
    use the selectedCityWeather state of weatherStore to access the specific city data and override that using the route.params.city and reload the city weather api

--- src/views/CityInfo.vue
+++ src/views/CityInfo.vue
@@ -1,19 +1,35 @@
 <template>
   <Navbar />
-  <div class="p-8">
-    <h1 class="text-3xl font-bold mb-4">City Info: {{ data?.name || 'Not Found' }}</h1>
-    <pre>{{ data }}</pre> 
+  <div v-if="cityWeather">
+    <pre>{{ cityWeather }}</pre>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
+import { useWeatherStore } from '@/stores/weatherStore'
+import { onMounted, computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Navbar from '@/components/Navbar/Navbar.vue'
 
-const props = defineProps({
-  city: String,
-  data: Object
-})
+const weatherStore = useWeatherStore()
+const route = useRoute()
 
-console.log(props)
+const cityWeather = computed(() => weatherStore.selectedCityWeather)
+
+// Fetch the weather data for the city when the component is mounted
+onMounted(async () => {
+  const cityName = route.params.city   // fetch the city in the url (this is the :city in path)
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY // fetch the api key since the process here is to reload the specific city weather
+  const country = 'PH' // hardcoded for a now since the default registered cities are in PH (philippines)
+
+  try {
+    // fetch the city information
+    const weatherData = await weatherStore.loadCityWeather(country, cityName, apiKey)
+    // Store the fetched data in the store
+    weatherStore.setSelectedCityWeather(weatherData) 
+  } catch (error) {
+    // throw an error in case the fethcing is not successful
+    console.error('error fetching city weather', cityWeather)
+  }
+})
 </script>
