5bfd8dbe2b9e889a7e1b81af8ec155bf8a336448
Mark Ciril <markciril.jamon@gmail.com>
2024-12-19 11:05:32 +0800

    FEAT: add hover event that change the background and cursor design

--- src/components/navbar/NavbarCard.vue
+++ src/components/navbar/NavbarCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid h-full w-full place-items-center rounded-lg border-2 p-1">
+    <div class="grid h-full w-full place-items-center rounded-lg border-2 p-1 hover:bg-gray-200 hover:cursor-pointer">
       <slot></slot>
     </div>
   </template>
