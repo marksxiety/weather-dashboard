ca8bdbfaae33596010fbc2c7769a063ba3253150
Mark Ciril <markciril.jamon@gmail.com>
2024-12-25 18:25:38 +0800

    style: card glassmorphism with border

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4 bg-background">
+  <div class="flex flex-col h-full w-full rounded-lg border-2 p-4  bg-gray-600  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-gray-100">
     <slot></slot>
   </div>
 </template>
