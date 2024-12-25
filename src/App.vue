f426b337ed6ce4b9fce313c5a063aa048e97c7a3
Mark Ciril <markciril.jamon@gmail.com>
2024-12-25 16:13:17 +0800

    chore: update the default background to gradient color

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="font-sans min-h-screen h-full w-full bg-background text-black">
+  <main class="font-sans min-h-screen h-full w-full bg-gradient-to-tr from-[#171717] to-[#3D3D3D] text-white">
     <RouterView />
   </main>
 </template>
