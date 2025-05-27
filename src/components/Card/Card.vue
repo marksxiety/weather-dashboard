120435034a206d7076bd1e6688cf384ceda79f39
Mark Ciril <markciril.jamon@gmail.com>
2025-05-28 05:26:03 +0800

    style: update Card component to use dynamic cursor styling based on props

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="flex flex-col h-full w-full rounded-lg p-4 bg-white/5 shadow-lg backdrop-blur-sm hover:cursor-pointer hover:shadow-none">
+  <div :class="['flex flex-col h-full w-full rounded-lg p-4 bg-white/5 shadow-lg backdrop-blur-sm hover:shadow-none', 'hover:cursor-' + pointerhover]">
     <slot></slot>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  pointerhover: {
+    type: String,
+    default: 'pointer'
+  }
+})
+</script>
