2defcfd339f5d11925403924f05b639402c44ab8
Mark Ciril <markciril.jamon@gmail.com>
2025-05-28 05:25:44 +0800

    style: update App component background gradient for improved visual appeal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="font-sans min-h-screen h-full w-full bg-gradient-to-tr from-[#171717] to-[#3D3D3D] text-white">
+  <main class="font-sans min-h-screen h-full w-full bg-gradient-to-br from-blue-50 to-indigo-100">
     <RouterView />
   </main>
 </template>
